cc0da2afcd2f844fa63702f796a4309eb8f3bf45
duyle1996 <duylebkdn@gmail.com>
2018-10-02 15:07:11 +0700

    buoi thu 9

---
+++ HTML/2018-09-28/web.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#js-navigation-button").click(function(){
+        $("#js-navigation").slideToggle();
+    });
+});
+// optional
+$('#blogCarousel').carousel({
+    interval: 5000
+});
