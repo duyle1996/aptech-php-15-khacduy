90995d2643418c53259de3989fdbd0f7c1146d42
duyle1996 <duylebkdn@gmail.com>
2018-10-23 16:33:51 +0700

    buoi 1 PHP

---
+++ PHP/2018-10-22/index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body>
+     
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Type</th>
+      <th>Price</th>
+      <th>Size</th>
+      <th>Ram</th>
+
+      <th>made In</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+$data = [
+    [
+        'type' => 'iphone',
+        'property' => [
+            'price' => 100,
+            'size' => 6.5,
+            'ram' => '2 GB',
+            'made' => [
+                'by' => 'California',
+                'in' => 'China',
+            ],
+        ],
+    ],
+    [
+        'type' => 'ipad',
+        'property' => [
+            'price' => 190,
+            'size' => 7.9,
+            'ram' => '4 GB',
+            'made' => [
+                'by' => 'California',
+                'in' => 'China',
+            ],
+        ],
+    ],
+];
+
+foreach ($data as $x => $x_value) {
+    echo '<tr>
+
+    <th>' . $x_value['type'] . '</th>
+    <th>' . $x_value['property']['price'] . '</th>
+    <th>' . $x_value['property']['size'] . '</th>
+    <th>' . $x_value['property']['ram'] . '</th>
+
+    <th>' . $x_value['property']['made']['in'] . '</th>
+
+
+    </tr>';
+  }
+
+?>
+  </tbody>
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  </body>
+</html>
